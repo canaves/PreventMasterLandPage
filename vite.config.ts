8dfbe8ab75e3b889e011aeea06f88bd0ed4e3775
Renan Canavês <renancanaves@gmail.com>
2026-07-30 23:40:21 -0300

    Configura GitHub Pages para novo repositorio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_PAGES === "true" ? "/master-lead-pro/" : "/",
+  base: process.env.GITHUB_PAGES === "true" ? "/PreventMasterLandPage/" : "/",
   server: {
     host: "::",
     port: 8080,
